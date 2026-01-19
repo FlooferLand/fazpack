8d2769989a7ef26025fcd48528ff75ed75c94dd5
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2026-01-19 20:39:43 +0200

    Add some blocks to the mod

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# hytale-fnaf
-FNaF mod for Hytale
+# Fazpack
+
+In-development FNaF mod for Hytale
+
+Made by FlooferLand with help from Development Multiverse
---
+++ src/main/java/com/flooferland/fazpack/FazpackPlugin.java
@@ -0,0 +1,18 @@
+package com.flooferland.fazpack;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+public class FazpackPlugin extends JavaPlugin {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public FazpackPlugin(JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Initialized %s v%s", getName(), getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+    }
+}
